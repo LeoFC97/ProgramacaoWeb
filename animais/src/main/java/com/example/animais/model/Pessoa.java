797b8afa77170fbfe540d1ae393feb0d43de3dbe
Leonardo <leonardo@Leonardos-MacBook-Pro.local>
2020-10-07 22:22:07 -0300

    feat: implements interface Pessoa

---
+++ animais/src/main/java/com/example/animais/model/Pessoa.java
@@ -0,0 +1,12 @@
+package com.example.animais.model;
+
+public interface Pessoa {
+    public String email="";
+    public String cpf = "";
+
+    public String getEmail();
+    public boolean setEmail(String email);
+    public String getCpf();
+    public boolean setCpf(String cpf);
+
+}
